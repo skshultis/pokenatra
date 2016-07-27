092ce980252b1fdfc249ef38417e37163656500a
Sarah K Shultis <skshultis@Sks-MacBook-Air.local>
2016-07-27 11:07:18 -0400

    MVP READY BABY

--- app.rb
+++ app.rb
@@ -16,7 +16,33 @@ get '/pokemons' do
     erb :"pokemons/index"
 end
 
+get '/pokemons/new' do
+  erb :"pokemons/new"
+end
+
 get '/pokemons/:id' do
   @pokemon = Pokemon.find(params[:id])
   erb :"pokemons/show"
+end
+
+post '/pokemons' do
+  pokemon = Pokemon.create(params[:pokemon])
+  redirect "pokemons/#{pokemon.id}"
+end
+
+get '/pokemons/:id/edit' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemons/edit"
+end
+
+put '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.update(params[:pokemon])
+  redirect "pokemons/#{@pokemon.id}"
+end
+
+delete '/pokemons/:id' do
+  pokemon = Pokemon.find(params[:id])
+  pokemon.destroy
+  redirect '/pokemons'
 end
